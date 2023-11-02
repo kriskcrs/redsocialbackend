4a16887fa7e15e51bbf5ec88b3af938c9071866e
Pablin117 <tgelma117@gmail.com>
2023-11-01 20:57:59 -0600

    servicios de publicacion

--- src/main/java/com/desarrolloweb/redsocial/Repository/PublicationRepository.java
+++ src/main/java/com/desarrolloweb/redsocial/Repository/PublicationRepository.java
@@ -2,5 +2,5 @@ package com.desarrolloweb.redsocial.Repository;
 import com.desarrolloweb.redsocial.Entity.Publication;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface PublicationRepository extends JpaRepository<Publication,Integer>{
-    public Publication findByIdUser(String idUser);
+    public Publication findByUserIdUser(String idUser);
 }
--- src/main/java/com/desarrolloweb/redsocial/Service/PublicationService.java
+++ src/main/java/com/desarrolloweb/redsocial/Service/PublicationService.java
@@ -1,5 +1,6 @@
 package com.desarrolloweb.redsocial.Service;
 import com.desarrolloweb.redsocial.Entity.Publication;
+import com.desarrolloweb.redsocial.Entity.User;
 import com.desarrolloweb.redsocial.Tools.Encoding;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,9 +25,51 @@ public class PublicationService {
     @Autowired
     PublicationRepository publicationRepository;
 
+    HashMap<String, String> response = new HashMap<>();
     @GetMapping(path = "/publications")
     private ResponseEntity<List<Publication>> publicationList() {
         return ResponseEntity.ok(publicationRepository.findAll());
+    }
+
+    //Crea  publicacion
+    @PostMapping(path = "/createPublication")
+    private ResponseEntity<HashMap<String, String>> createPublication(@RequestBody Publication publication){
+        if(publication != null) {
+            int idPublication = publicationRepository.findAll().size();
+            idPublication++;
+            publication.setIdPublication(idPublication);
+            publication.setCreationDate(new Date());
+            publicationRepository.save(publication);
+            response.put("message", "Publicacion Creada");
+            return ResponseEntity.ok(response);
+        }else{
+            response.put("message", "Llenar todos los campos");
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    //Modifica Publicacion
+    @PutMapping(path = "/modifyPublication")
+    private ResponseEntity<HashMap<String, String>> modifyPublication(@RequestBody Publication publication){
+        if(publication != null) {
+            publication.setModificationDate(new Date());
+            publicationRepository.save(publication);
+            response.put("message", "Publicacion Modificada");
+            return ResponseEntity.ok(response);
+        }else{
+            response.put("message", "Llenar todos los campos");
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+
+    //Elimina  publicacion
+    @DeleteMapping(path = "/delete/publication/{id}")
+    private ResponseEntity<HashMap<String, String>> deletePublication(@PathVariable int id) {
+        response.clear();
+        publicationRepository.deleteById(id);
+        response.put("message", "Publicacion eliminada");
+        return ResponseEntity.ok(response);
     }
 
 }
